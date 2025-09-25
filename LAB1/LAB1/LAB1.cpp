55c72156f4434c7443ed7dcec8a38daeac6de34a
JesusKill13 <r.lysicov2007@gmail.com>
2025-09-25 15:18:36 +0200

    30

---
+++ LAB1/LAB1/LAB1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int x;
+    int y;
+    int z;
+    cout << "Введите первый катет: ";
+    cin >> x;
+    cout << "Введите второй катет: ";
+    cin >> y;
+    z = pow(pow(x, 2) + pow(y, 2), 0.5);
+    cout << "Гипотенуза = " << z << endl;
+    cout << "Периметр треугольника = " << x + y + z;
+    return 0;
+    
+    
+}
